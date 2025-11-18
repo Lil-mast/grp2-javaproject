149dee72f51861727bd81b1dbb2b9b5d7170211b
leo001-z <lennykim001@gmail.com>
2025-11-18 12:50:38 +0300

    Update ArrayOperations.java

--- arrays/ArrayOperations.java
+++ arrays/ArrayOperations.java
@@ -24,3 +24,34 @@ public class ArrayOperations {
         System.out.println("Sum of all elements: " + sum);
     }
 }
+
+        // 2. SEARCH using iteration
+        System.out.print("Enter number to search: ");
+        int target = sc.nextInt();
+        boolean found = false;
+        for (int num : numbers) {
+            if (num == target) {
+                found = true;
+                break;
+            }
+        }
+        System.out.println(found ? "Number found!" : "Number not found.");
+        // 3. MODIFY using iteration
+        System.out.print("Enter number to modify: ");
+        int oldVal = sc.nextInt();
+        System.out.print("Enter new value: ");
+        int newVal = sc.nextInt();
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == oldVal) {
+                numbers[i] = newVal;
+                System.out.println("Value updated.");
+                break;
+}
+        }
+        // Display modified array
+        System.out.print("Updated array: ");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
+}
